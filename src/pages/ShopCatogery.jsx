fcdb77553965547c85c3ab9fb83ebe960d5b19e3
Abdulrahman Omar <aaomerali@gmail.com>
2024-12-24 12:04:35 +0300

    change ShopCatogery component

--- src/pages/ShopCatogery.jsx
+++ src/pages/ShopCatogery.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
+import Item from '../components/Item'
+
 
 function ShopCatogery() {
   return (
-    <div>ShopCatogery</div>
+
+    <div className='w-full flex justify-center py-20'>
+
+        <div className="container  px-8  p-5 text-center relative">=
+
+          <div className='flex items-center justify-center flex-wrap gap-8 mb-10 '>
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+            <Item />
+          </div>
+        
+        </div>
+    </div>
   )
 }
 
